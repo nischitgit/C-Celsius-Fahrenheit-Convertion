dcef62000c9b7bec5a94ac88e8337ad1a764a409
Nischit <123927039+NischitGit@users.noreply.github.com>
2023-01-30 15:16:15 +0530

    Update celsius_to_fahrenheit.c

--- celsius_to_fahrenheit.c
+++ celsius_to_fahrenheit.c
@@ -3,14 +3,14 @@
 
 int
 main () {
-  // Variable
-  float celsius;
+  // Variables
+  float celsius, fahrenheit;
   // Instruction
   printf ("Celsius to Fahrenheit \n");
   printf ("Enter Celsius: ");
   scanf ("%f", &celsius);
   // Output
-  float fahrenheit = celsius * 9 / 5 + 32;
+  fahrenheit = celsius * 9 / 5 + 32;
   printf ("Fahrenheit: %f", fahrenheit);
   return 0;
 }
