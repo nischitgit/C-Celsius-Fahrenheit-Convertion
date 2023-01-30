a496535fa13e53c6833c005d721d8801b0e74edd
Nischit <123927039+NischitGit@users.noreply.github.com>
2023-01-30 12:28:34 +0530

    Create fahrenheit_to_celsius.c

---
+++ fahrenheit_to_celsius.c
@@ -0,0 +1,17 @@
+// Fahrenheit to Celsius
+#include <stdio.h>
+
+int
+main ()
+{
+  // Variable
+  float fahrenheit;
+  // Instruction
+  printf ("Fahrenheit to Celsius \n");
+  printf ("Enter Fahrenheit: ");
+  scanf ("%f", &fahrenheit);
+  // Output
+  float celius = (fahrenheit - 32) * 5 / 9;
+  printf ("Celsius: %f", celius);
+  return 0;
+}
